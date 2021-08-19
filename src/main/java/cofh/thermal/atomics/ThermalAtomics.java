243e5607202356d2c4e69005d101c8cbf47129c3
King Lemming <kinglemming@gmail.com>
2021-08-19 10:46:59 -0700

    Refactor Core module workspace.
    Signed-off-by: King Lemming <kinglemming@gmail.com>

--- src/main/java/cofh/thermal/atomics/ThermalAtomics.java
+++ src/main/java/cofh/thermal/atomics/ThermalAtomics.java
@@ -5,6 +5,8 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 import static cofh.lib.util.constants.Constants.ID_THERMAL_ATOMICS;
+import static cofh.thermal.lib.common.ThermalFlags.FLAG_NUCLEAR_EXPLOSIVES;
+import static cofh.thermal.lib.common.ThermalFlags.setFlag;
 
 @Mod(ID_THERMAL_ATOMICS)
 public class ThermalAtomics {
@@ -18,6 +20,7 @@ public class ThermalAtomics {
 
     private void setFeatureFlags() {
 
+        setFlag(FLAG_NUCLEAR_EXPLOSIVES, true);
     }
 
 }
